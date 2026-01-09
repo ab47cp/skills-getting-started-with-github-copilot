53b1340f54bb57be77e9912342d81c1040579bbf
Aman Kumar <93607637+ab47cp@users.noreply.github.com>
2026-01-09 04:50:34 +0000

    Clear dropdown options when fetching activities

--- src/static/app.js
+++ src/static/app.js
@@ -12,6 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Clear loading message
       activitiesList.innerHTML = "";
+      // Clear dropdown options except the first one
+      activitySelect.innerHTML = '<option value="">-- Select an activity --</option>';
 
       // Populate activities list
       Object.entries(activities).forEach(([name, details]) => {
